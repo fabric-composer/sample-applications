550f3a765ffc69fb3c9ae84ae1f460dde3d4c6d3
Liam Grace <liamgrace.896@gmail.com>
2017-04-11 13:14:12 +0100

    Removed kill all docker images command

--- packages/getting-started/scripts/start-hyperledger.sh
+++ packages/getting-started/scripts/start-hyperledger.sh
@@ -10,7 +10,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 cd "${DIR}"/hlfv1
 
 docker-compose -f hlfv1_alpha-docker-compose.yml down
-docker rm $(docker ps -aq) ||
+docker-compose -f hlfv1_alpha-docker-compose.yml kill
 docker-compose -f hlfv1_alpha-docker-compose.yml up -d
 
 node create-channel.js
