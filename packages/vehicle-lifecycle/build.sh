ce8d21f7481f99aa25adc54b66d950f6a4ff87eb
Jake Turner <jaketurner25@live.com>
2017-06-19 16:23:16 +0100

    Updated Vehicle-Lifecycle to use Hyperledger Fabric v1.0.0-beta

--- packages/vehicle-lifecycle/build.sh
+++ packages/vehicle-lifecycle/build.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"
 cp -f install.sh.in install.sh
 echo "PAYLOAD:" >> install.sh
-tar czf - .composer-connection-profiles .hfc-key-store crypto-config docker-compose.yml flows.json mychannel.tx twoorgs.orderer.block vehicle-lifecycle-network.bna >> install.sh
+tar czf - .composer-connection-profiles .hfc-key-store crypto-config docker-compose.yml flows.json mychannel.tx twoorgs.genesis.block vehicle-lifecycle-network.bna >> install.sh
