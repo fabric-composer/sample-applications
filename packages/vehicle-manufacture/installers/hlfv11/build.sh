1347fd5ea6e447610fa900e59ef46657e5fbd14a
Robert Miroballi <rmiroballi@gmail.com>
2018-04-06 14:57:04 -0500

    Change VEHICLE-LIFECYCLE-VERSION to VEHICLE-MANUFACTURE-VERSION
    Updated to work with PR#112

--- packages/vehicle-manufacture/installers/hlfv11/build.sh
+++ packages/vehicle-manufacture/installers/hlfv11/build.sh
@@ -21,7 +21,7 @@ npm install vehicle-manufacture-network@~0.2.1 --no-save
 cd "${DIR}"
 cat install.sh.in | sed \
     -e 's/{{COMPOSER-VERSION}}/next/g' \
-    -e 's/{{VEHICLE-LIFECYCLE-VERSION}}/latest/g' \
+    -e 's/{{VEHICLE-MANUFACTURE-VERSION}}/latest/g' \
     -e 's/{{NODE-RED-VERSION}}/next/g' \
     > install.sh
 echo "PAYLOAD:" >> install.sh
